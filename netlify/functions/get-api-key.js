09bdf2cdfede34232b0c01c6d3e745f5600085e5
gamehov <gamehov.com@gmail.com>
2025-02-03 20:05:47 -0500

    Update get-api-key.js

--- netlify/functions/get-api-key.js
+++ netlify/functions/get-api-key.js
@@ -1,9 +1,15 @@
-// functions/get-api-key.js
-exports.handler = async (event, context) => {
+// .netlify/functions/get-api-key.js
+exports.handler = async function(event, context) {
+    const API_KEY = process.env.FREETOGAME_API_KEY; // Set your RapidAPI key in the environment variables
+    if (!API_KEY) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "API key is missing in environment variables." })
+        };
+    }
+
     return {
-      statusCode: 200,
-      body: JSON.stringify({
-        API_KEY: process.env.FREETOGAME_API_KEY, // Ensure this matches the environment variable name
-      }),
+        statusCode: 200,
+        body: JSON.stringify({ API_KEY })
     };
-  };
+};
